80e9c0cc6dec59b31f353ec2fe0e4e9182654c1a
Alexandra <andonova.alexandra@gmail.com>
2021-01-09 16:37:49 +0200

    test api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(
+    private http: HttpClient, 
+    private route: ActivatedRoute
+    ) {
+}
+ngOnInit(): void {
+  throw new Error("Method not implemented.");
+}
 }
--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { Member } from '../models/Member';
 
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
   styleUrls: ['./member.component.css']
 })
+
 export class MemberComponent implements OnInit {
 
-  constructor() { }
+  member: Member;
 
-  ngOnInit(): void {
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute)
+    { 
+      this.member = new Member();
+    }
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.http.get(`/api/members/`).subscribe(res => {
+        this.member = res as Member;
+      });
+    });
   }
-
 }
+
+
